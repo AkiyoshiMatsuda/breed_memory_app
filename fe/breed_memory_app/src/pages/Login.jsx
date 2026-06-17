e900b936ffdcce86d86c400984f3706905c5dc59
AkiyoshiMatsuda <aki_01_28@icloud.com>
2026-06-17 16:32:39 +0900

    ログインのモックアップを作成->Login.jsx

--- fe/breed_memory_app/src/pages/Login.jsx
+++ fe/breed_memory_app/src/pages/Login.jsx
@@ -1,10 +1,16 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
 
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
+    console.log({ email, password });
 
     // 今はAPI未接続なので仮でホームへ
     navigate("/home");
@@ -15,17 +21,28 @@ function Login() {
       <h1>ログイン</h1>
 
       <form onSubmit={handleLogin} className="form">
-        <input type="email" placeholder="メールアドレス" />
-        <input type="password" placeholder="パスワード" />
-
+        <input
+          type="email"
+          placeholder="メールアドレス"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="パスワード"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">ログイン</button>
       </form>
-
+      
       <p>
         アカウントがない場合は <Link to="/register">新規登録</Link>
       </p>
     </div>
   );
+  console.log("Email:", email);
+  console.log("Password:", password);
 }
 
 export default Login;
